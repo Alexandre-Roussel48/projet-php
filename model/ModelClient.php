e76952043158f548329091ffdbe7ba6d9a0fae48
fressardm <fressardmelanie@gmail.com>
2021-11-08 14:25:31 +0100

    correction model client

--- model/ModelClient.php
+++ model/ModelClient.php
@@ -1,28 +1,28 @@
 <?php
 require_once File::build_path(array("model","Model.php"));
-class ModelP_client {
+class ModelClient {
     private $nom, $prenom, $mail, $mdp, $adresse;
 
 
     public function __construct($codeClient, $nom, $prenom, $mail, $mdp, $adresse=NULL){
-            if(!is_null(adresse)){
-                $this.adresse = $adresse;
-            }
-            $this.nom = $nom;
-            $this.prenom = $prenom;
-            $this.mail = $mail;
-            $this.mdp = $mdp;
+        if(!is_null($adresse)){
+            $this->adresse = $adresse;
+        }
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->mail = $mail;
+        $this->mdp = $mdp;
     }
 
 
     public function get($nomChamp){
-        return $this->nomChamp;
+        return $this->$nomChamp;
     }
 
-    
+
     public function set($nomChamp, $valeur){
         if($valeur!=="codeClient"){
-            $this->nomChamp = $valeur;
+            $this->$nomChamp = $valeur;
         }
     }
 
