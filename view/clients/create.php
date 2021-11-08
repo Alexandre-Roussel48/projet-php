6cbceced898e03e80917cb7b1cb90da8d55fa2f9
fressardm <fressardmelanie@gmail.com>
2021-11-08 16:46:50 +0100

    correction des champs et ajout dune couleur de fond

--- view/clients/create.php
+++ view/clients/create.php
@@ -4,29 +4,27 @@
         <input type='hidden' name='action' value='created'>
 
         <fieldset>
-            <legend>Obligatoire : </legend>
+            <legend>Informations obligatoires : </legend>
             <p>
-                <label for="nom">Nom*</label> :
+                <label for="nom">Nom</label> :
                 <input type="text" placeholder="Gaston" id="nom" required/>
             </p>
-
             <p>
-                <label for="prenom">Prénom*</label> :
+                <label for="prenom">Prénom</label> :
                 <input type="text" placeholder="Lagaffe" name="prenom" id="prenom" required/>
             </p>
-
             <p>
-                <label for="mail">Adresse email*</label> :
+                <label for="mail">Adresse email</label> :
                 <input type="text" placeholder="gaston.lagaffe@tutanota.com" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
                        name="mail" id="mail" required/>
             </p>
             <p>
-                <label for="mdp">Mot de passe*</label> :
+                <label for="mdp">Mot de passe</label> :
                 <input type="password" name="mdp" id="mdp" required/>
             </p>
         </fieldset>
         <fieldset>
-            <legend>Pas obligatoire </legend>
+            <legend>Informations complémentaires :</legend>
             <p>
                 <label for="adresse">Adresse</label> :
                 <input type="text" placeholder="1 rue du sport 34000 Montpellier" name="adresse" id="adresse" required/>
@@ -34,7 +32,6 @@
         </fieldset>
 
         <p>
-
             <input type="submit" value="Envoyer" />
         </p>
     </fieldset>
