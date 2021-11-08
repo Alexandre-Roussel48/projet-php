09129925965421c041bfbaceb664745ac7678381
fressardm <fressardmelanie@gmail.com>
2021-11-08 15:15:29 +0100

    ajout de la feuille de style

--- view/view.php
+++ view/view.php
@@ -3,18 +3,22 @@
     <head>
         <meta charset="UTF-8">
         <title><?php echo $pagetitle; ?></title>
+        <link rel="stylesheet" type="text/css" href="./view/css/style.css">
     </head>
     <body>
-        <a href="index.php?controller=modeles&action=readAll">Tout les modèles</a>
-        <a href="index.php?controller=utilisateur&action=readAll">Page d'accueil utilisateur</a>
+        <nav>
+            <a href="index.php?controller=modeles&action=readAll">Tout les modèles</a>
+            <a href="index.php?controller=utilisateur&action=readAll">Page d'accueil utilisateur</a>
+        </nav>
+
 <?php
 $filepath = File::build_path(array("view", $controller, "$view.php"));
 require $filepath;
 ?>
     </body>
     <footer>
-        <p style="border: 1px solid black;text-align:right;padding-right:1em;">
-        Site d' e-commerce de Gatien, Mélanie, Bastien et Alexandre !
+        <p>
+            Site d' e-commerce de Gatien, Mélanie, Bastien et Alexandre !
         </p>
     </footer>
 </html>
