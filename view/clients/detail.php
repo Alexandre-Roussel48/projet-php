8ddb0a33a32e9847c15cabb22bbad02d6dd8e6a6
Gatien <gatien.depeyre@etu.umontpellier.fr>
2021-11-10 08:38:44 +0100

    detail client fonctionnel

--- view/clients/detail.php
+++ view/clients/detail.php
@@ -1,15 +1,15 @@
 <?php
-    $codeId = htmlspecialchars($p_m->get('codeClient')); 
+    $codeId = htmlspecialchars($c->get('codeClient')); 
 
-    $nom = htmlspecialchars($p_m->get('nomClient')); 
+    $nom = htmlspecialchars($c->get('nomClient')); 
 
-    $prenom = htmlspecialchars($p_m->get('prenomClient')); 
+    $prenom = htmlspecialchars($c->get('prenomClient')); 
     
-    $mail = htmlspecialchars($p_m->get('mail')); 
+    $mail = htmlspecialchars($c->get('mail')); 
     
-    $adresse = htmlspecialchars($p_m->get('adresse')); 
+    $adresse = htmlspecialchars($c->get('adresse')); 
 
-    $telephone = htmlspecialchars($p_m->get('telephone')); 
+    $telephone = htmlspecialchars($c->get('telephone')); 
 
     //$prixHTML = htmlspecialchars($prix);
     echo "Utilisateur : {$codeId}, {$nom}, {$prenom}, {$mail}, {$adresse}, {$telephone}";
