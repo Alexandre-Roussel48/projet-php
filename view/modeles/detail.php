20630de16693555d7f1a8b8120ee1f977532303a
BastienGavioli <bastien.gavioli@etu.umontpellier.fr>
2021-11-20 23:34:56 +0100

    Modification de la vue de modèles

--- view/modeles/detail.php
+++ view/modeles/detail.php
@@ -3,14 +3,21 @@
     $modele = $m->get('modele'); $modeleHtml = htmlspecialchars($modele);
     $marque = $m->get('marque'); $marqueHtml = htmlspecialchars($marque);
     $prix = $m->get('prix'); $prixHtml = htmlspecialchars($prix);
-    
-    echo "<p> Modèle : {$modeleHtml} Marque : {$marqueHtml} Prix : {$prixHtml} € </p>";
-
+?>
+<div style="text-align: center;">
+    <span style="margin-bottom: auto; font-weight: bold; text-transform: uppercase; font-size: 24px;"><?php echo "$marqueHtml"?></span>
+        <span style="margin: auto;  font-size: 24px;"> - <em><?php echo "$modeleHtml"?></em></span>
+        <p style="margin: auto; font-size: 28px;">Prix : <?php echo "$prixHtml"?></p>
+</div>
+    <?php echo "<p>Nombre de produits disponibles : ".count($tab_p)."</p>";?>
+<ul>
+    <?php
     foreach ($tab_p as $p) {
         $stock = $p->get('stock');  $stockHtml = htmlspecialchars($stock);
         $taille = $p->get('taille');    $tailleHtml = htmlspecialchars($taille);
         $couleur = $p->get('couleur');  $couleurHtml = htmlspecialchars($couleur);
-        echo "<p> Quantité : {$stockHtml} Taille : {$tailleHtml} pouces Couleur : {$couleurHtml} </p>";
-        echo "<a>Ajouter au panier</a>";
+        echo "<li> Quantité restante : {$stockHtml}. Taille : {$tailleHtml} pouces. Couleur : {$couleurHtml}. ";
+        echo "<p><a>Ajouter au panier</a></li></p>";
     }
-?>
+    ?>
+</ul>
