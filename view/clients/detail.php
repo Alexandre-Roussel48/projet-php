86318f5fa5a17fd8ffe737f61b6dde569308b0c1
gaviolib <bastien.gavioli@etu.umontpellier.fr>
2021-11-29 15:01:23 +0100

    Modification Mon profil

--- view/clients/detail.php
+++ view/clients/detail.php
@@ -4,7 +4,7 @@
         $codeId = htmlspecialchars($c->get('codeClient'));
         $nom = htmlspecialchars($c->get('nomClient'));
         $prenom = htmlspecialchars($c->get('prenomClient')); 
-        $mail = htmlspecialchars($c->get('mail')); 
+        $mail = htmlspecialchars($c->get('mail'));
         $adresse = htmlspecialchars($c->get('adresse')); 
         $telephone = htmlspecialchars($c->get('telephone')); 
 
@@ -12,8 +12,14 @@
         echo "<p>Nom : {$nom}</p>";
         echo "<p>Prénom : {$prenom}</p>";
         echo "<p>Adresse email utilisée : {$mail}</p></fieldset>";
-        echo "<fieldset><p>Adresse : </p><p>{$adresse}</p>";
-        echo "<p>Téléphone : {$telephone}</p></fieldset>";
+        echo "<fieldset><p>Adresse : ";
+        if($c->get('adresse')!=="") 
+            echo "</p><p>{$adresse}</p>";
+        else
+            echo "<em>non renseignée</em></p>";
+        if($c->get('telephone')!=="")
+            echo "<p>Téléphone : {$telephone}</p></fieldset>";
+        else echo "<p>Téléphone : <em>non renseignée</em></p></fieldset>";
         if(ModelClients::isAdmin($c->get('mail')))
             echo '<p style="color: red;">Possède le rôle Administateur</p>';
     ?>
