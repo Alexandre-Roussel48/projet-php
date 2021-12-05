d9f3bd043eebda8bde448eb087c0b731b649880f
BastienGavioli <bastien.gavioli@etu.umontpellier.fr>
2021-12-05 16:01:01 +0100

    Affichage de tous les modèles correct

--- view/modeles/list.php
+++ view/modeles/list.php
@@ -1,12 +1,16 @@
 
 <?php
+echo '<div class="modelesWrapper">';
 foreach ($tab_mod as $m) {
     $modele = $m->get('modele');
+    $marque = $m->get('marque');
     $modeleHtml = htmlspecialchars($modele);
+    $marqueHtml = htmlspecialchars($marque);
     $modeleUrl = rawurlencode($modele);
-    echo '<div class="modelContainer"><a href="?controller=modeles&action=read&modele='.$modeleUrl.'">';
+    echo '<div class="modelesContainer"><a href="?controller=modeles&action=read&modele='.$modeleUrl.'">';
     echo '<img src="./annexes/ressources/'.$m->get('image');
     echo '" alt="Le '.$m->get('marque').' '.$m->get('modele').'" width="200" height="120" />';
-    echo "<p>{$modeleHtml}</p></a></div>";
+    echo "<p>{$marqueHtml} - {$modeleHtml}</p></a></div>";
 }
+echo '</div>'
 ?>
