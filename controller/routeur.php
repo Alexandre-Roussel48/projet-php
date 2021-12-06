6865f6356f99b47bbd48d8ac678bb108e098844b
Gatien <gatien.depeyre@etu.umontpellier.fr>
2021-12-06 17:47:11 +0100

    Verification de l'existance du controlleur et de la méthode

--- controller/routeur.php
+++ controller/routeur.php
@@ -4,15 +4,23 @@ require_once File::build_path(array("controller", "ControllerClients.php"));
 require_once File::build_path(array("controller", "ControllerProduits.php"));
 
 session_start();
-
-if(isset($_GET['action'])&&isset($_GET['controller'])) {
+//Verifie si le controller est remplis et que la classe associé existe
+if(isset($_GET['controller']) && class_exists('Controller'.$_GET['controller'])) {
 	$controller = $_GET['controller'];
-	$action = $_GET['action'];
+
+	//Maintenant on vérifie que l'action est remplie et que la méthode associé existe
+	if(isset($_GET['action']) && method_exists('Controller'.$controller, $_GET['action'])){
+		$action = $_GET['action'];
+	} else{
+		$action = "readAll";
+	}
 }
 else {
 	$controller = "modeles";
 	$action = "readAll";
 }
+
+
 
 
 $controllerName = "Controller".ucfirst($controller);
