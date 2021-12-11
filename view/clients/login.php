993c646df6ad81d324604de998f264809c0e2e98
BastienGavioli <bastien.gavioli@etu.umontpellier.fr>
2021-12-11 23:22:03 +0100

    Correction balises html login

--- view/clients/login.php
+++ view/clients/login.php
@@ -13,7 +13,7 @@
 
         <label for="mail">
             Adresse email :
-            <input type="text" placeholder="gaston.lagaffe@tutanota.com" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" name="mail"
+            <input type="text" placeholder="gaston.lagaffe@tutanota.com" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" name="mail" id="mail"
                 <?php
                     if(isset($email)) echo ' value="'.$email.'"';
                 ?> required/>
@@ -21,19 +21,18 @@
 
         <label for="mdp">
             Mot de passe :
-            <input type="password" name="mdp"
+            <input type="password" name="mdp" id="mdp"
                 <?php
                     if(isset($mdp)) echo 'value="'.$mdp.'"';
                 ?>required/>
         </label>
+        <button type="submit">Connexion</button>
     </fieldset>
 
-        <button type="submit">Connexion</button>
-
-    <fieldset>
-        <h3>Vous n'avez pas de compte ou un compte non activé?</h3>
-        <p><a href="index.php?controller=clients&action=create" id="createAccount">Créer un compte</a></p>
-        <p><a href="index.php?controller=clients&amp;action=verifNonce" id="createAccount">Activer son compte</a></p>
+    <fieldset class="createAccount">
+        <h3>Vous n'avez pas de compte ou un compte non activé ?</h3>
+        <p><a href="index.php?controller=clients&action=create">Créer un compte</a></p>
+        <p><a href="index.php?controller=clients&action=verifNonce">Activer son compte</a></p>
         <?php
             if($_GET['action']=="verification") {
                 echo '<p style="color:red;">Votre email ou votre mot de passe est incorrect.</p>';
