a3ce9389a159f54fbb1fd85dd59b7b58138ceda7
Gatien <gatien.depeyre@etu.umontpellier.fr>
2021-12-11 10:58:51 +0100

    Correction mineure

--- view/produit/history.php
+++ view/produit/history.php
@@ -30,7 +30,6 @@ EOD;
             </table>";
     } else {
         echo <<<EOD
-            <h1>Historique de vos commandes</h1>
             <p>À notre connaissance, vous n'avez rien commandé</p>
 EOD;
 }
